f056f4902202ac3e7a6b64f7aadd6fc516cde128
후후 후후후후 <zhffl12@naver.com>
2016-11-03 16:55:53 +0900

    후후

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
   printf("Open Source SW Project\n");
   printf("first GitHub Tranning\n");
   printf("im jino..");
-  printf("2016 10 31");  
+  printf("2016 10 31");
+  printf("후후"); 
   return 0;
 }
